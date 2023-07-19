dcba6ce630c6d65bf663ef5ded06a9fd8126d972
Egor29312 <egorefremov064@gmail.com>
2023-07-19 22:59:27 +0300

    message from first.txt to second.txt

--- index.js
+++ index.js
@@ -3,3 +3,6 @@ const fs = require('fs');
 fs.writeFileSync('first.txt', 'Я люблю NodeJS');
 
 fs.writeFileSync('second.txt', 'I like NodeJS');
+
+const data = fs.readFileSync('first.txt', 'utf8');
+fs.appendFileSync('second.txt', data);
